9c3e76be80b15cc26f817075e87c70e8142117e1
Rajesh Kumar <rajeshkumar.thandapani@gmail.com>
2021-09-20 13:21:32 +0530

    Added AWS access and secret key

--- main.tf
+++ main.tf
@@ -18,9 +18,13 @@ terraform {
 # ------------------------------------------------------------------------------
 # CONFIGURE OUR AWS CONNECTION
 # ------------------------------------------------------------------------------
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 
 provider "aws" {
   region = "us-east-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
