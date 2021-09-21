be73a8d91c8d9e36266917abb0488ec3bd9b44c9
rajeshkumarblr <rajeshkumar.thandapani@gmail.com>
2021-09-21 06:02:26 +0530

    removed the t2.medium from for_each

--- main.tf
+++ main.tf
@@ -150,7 +150,6 @@ resource "aws_instance" "my-machine" {
   ami = "ami-0c55b159cbfafe1f0"
   for_each  = {                     # for_each iterates over each key and values
       key1 = "t2.micro"             # Instance 1 will have key1 with t2.micro instance type
-      key2 = "t2.medium"            # Instance 2 will have key2 with t2.medium instance type
         }
   instance_type  = each.value
   tags =  {
