3debe7f112630b854a9514e734d7c31b7eb71062
rajeshkumarblr <rajeshkumar.thandapani@gmail.com>
2021-09-27 05:26:16 +0530

    removed the subnet id and sec group

--- main.tf
+++ main.tf
@@ -33,9 +33,7 @@ locals {
       for i in range(1, srv.no_of_instances+1) : {
         instance_name = "${srv.application_name}-${i}"
         instance_type = srv.instance_type
-        subnet_id   = srv.subnet_id
         ami = srv.ami
-        security_groups = srv.vpc_security_group_ids
       }
     ]
   ]
